de3d2760365fc7611e968a51611a5b5608e284db
John Die <ajsds@gmail.com>
2024-08-31 10:27:36 -0500

    do while

--- GestionBiblioteca/src/gestionbiblioteca/GestionBiblioteca.java
+++ GestionBiblioteca/src/gestionbiblioteca/GestionBiblioteca.java
@@ -1,4 +1,3 @@
-
 package gestionbiblioteca;
 
 import java.util.Scanner;
@@ -9,12 +8,10 @@ import java.util.Scanner;
  */
 public class GestionBiblioteca {
 
-  
     public static void main(String[] args) {
         Biblioteca biblioteca = new Biblioteca();
         Scanner scanner = new Scanner(System.in);
-        
-        
+
         System.out.println("Bienvenido a Konrad Library");
         System.out.println("1) Prestamo de libros");
         System.out.println("2) Buscar libros por autor");
@@ -22,36 +19,35 @@ public class GestionBiblioteca {
         System.out.println("4) Devolver libro");
         System.out.println("5) Salir");
         System.out.println("Ingrese una opcion");
-        int opc = Integer.parseInt(scanner.nextLine());
-        switch(opc){
-        
-            case 1:
-                System.out.println("Ingrese el nombre del libro");
-                String titulo = scanner.nextLine();
-                biblioteca.prestarLibro(titulo);
-                break;
-            case 2: 
-                System.out.println("Ingrese el autor a buscar");
-                String autor = scanner.nextLine();
-                biblioteca.buscarPorAutor(autor);
-                
-                break;
-            case 3: 
-                System.out.println("");
-                break;
-            case 4: 
-                System.out.println("");
-                break;
-            case 5:
-                System.out.println("");
-                break;
-                
-        
+        while (true) {
+            int opc = Integer.parseInt(scanner.nextLine());
+            switch (opc) {
+
+                case 1:
+                    System.out.println("Ingrese el nombre del libro");
+                    String titulo = scanner.nextLine();
+                    biblioteca.prestarLibro(titulo);
+                    break;
+                case 2:
+                    System.out.println("Ingrese el autor a buscar");
+                    String autor = scanner.nextLine();
+                    biblioteca.buscarPorAutor(autor);
+
+                    break;
+                case 3:
+                    System.out.println("");
+                    break;
+                case 4:
+                    System.out.println("");
+                    break;
+                case 5:
+                    System.out.println("");
+                    break;
+
+            }
+
         }
+
     }
-    
-    
-    
-    
-    
+
 }
