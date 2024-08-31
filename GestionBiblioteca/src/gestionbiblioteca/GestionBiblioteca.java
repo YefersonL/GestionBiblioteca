17081421e61433bb90baf63908535d140adfe47a
nose <sapo@kopasksa.vo>
2024-08-31 10:20:56 -0500

    cambio en sitch

--- GestionBiblioteca/src/gestionbiblioteca/GestionBiblioteca.java
+++ GestionBiblioteca/src/gestionbiblioteca/GestionBiblioteca.java
@@ -14,17 +14,21 @@ public class GestionBiblioteca {
         Biblioteca biblioteca = new Biblioteca();
         Scanner scanner = new Scanner(System.in);
         
-        int opc = scanner.nextInt();
+        
         System.out.println("Bienvenido a Konrad Library");
         System.out.println("1) Prestamo de libros");
         System.out.println("2) Buscar libros por autor");
         System.out.println("3) Buscar libros por titulo");
         System.out.println("4) Devolver libro");
         System.out.println("5) Salir");
+        System.out.println("Ingrese una opcion");
+        int opc = scanner.nextInt();
         switch(opc){
         
             case 1:
-                System.out.println();
+                System.out.println("Ingrese el nombre del libro");
+                String titulo = scanner.nextLine();
+                biblioteca.prestarLibro(titulo);
                 break;
             case 2: 
                 System.out.println("Ingrese el autor a buscar");
